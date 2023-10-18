7f78c24ca6d672bd1ed8b92ab8d757592e6252ad
tanavishali <tanawishalrai5271@gmail.com>
2023-10-18 04:22:30 -0700

    first

---
+++ days in week.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int days=7,week=3;
+	for(int i=1;i<=week;i++){
+		cout<<i<<endl;
+		for(int j=1;j<=days;j++){
+			cout<<"day: "<<j<<endl;
+		}
+		
+	}
+	return 0;
+}
